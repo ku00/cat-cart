80c1a7e84e28db258c68ed9d8491d71979dff1c2
ku00 <skentaro36@gmail.com>
2017-11-19 21:49:27 +0900

    fix file name to fit to Angular Style Guide

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,8 +2,8 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  context: path.resolve(__dirname, '../app'),
-  entry: './src/app.module',
+  context: path.resolve(__dirname, '../src'),
+  entry: './app/main',
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
